d0949484b309a49ae64b7d36cc1846a4ac4f7e30
katrinmelody <77362916+katrinmelody@users.noreply.github.com>
2023-06-29 21:02:49 +0300

    Set up security middleware

--- src/app.js
+++ src/app.js
@@ -3,13 +3,38 @@ const express = require('express');
 const { auth } = require('express-openid-connect');
 const dotenv = require('dotenv');
 const { requiresAuth } = require('express-openid-connect');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const recipesRouter = require('./routers/recipesRouter');
 const AppError = require('./utils/appError');
 
 const app = express();
 
+//SECURITY MIDDLEWARES
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: ['search'],
+  }),
+);
+
 dotenv.config({ path: '.env' });
 
+//AUTH0 configuration
 const config = {
   authRequired: false,
   auth0Logout: true,
