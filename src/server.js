a550d382cf1024855715215f105f0ed5efd4c7b5
katrinmelody <77362916+katrinmelody@users.noreply.github.com>
2023-06-29 23:20:24 +0300

    Refactor

--- src/server.js
+++ src/server.js
@@ -1,42 +1,22 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const app = require('./app');
 
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
+  console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise);
+  console.error('Reason:', reason);
 });
 
 dotenv.config({ path: '.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<password>',
-  process.env.DATABASE_PASSWORD,
-);
-
 mongoose.set('strictQuery', false);
-
-mongoose.connect(DB).then(() => console.log('DB connected!'));
-
-const options = {
-  key: fs.readFileSync('/Users/kahutyryak/Projects/key.pem'),
-  cert: fs.readFileSync('/Users/kahutyryak/Projects/cert.pem'),
-};
+mongoose.connect(process.env.DATABASE).then(() => console.log('DB connected!'));
 
 const PORT = 3000;
-
-const server = https.createServer(options, app).listen(PORT, () => {
-  console.log(`app is running on port ${PORT}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`app is running on port ${PORT}`));
